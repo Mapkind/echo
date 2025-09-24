414ba65a37d93afb6dbd8b023ba4fb8a9ee97b0b
Mapkind <92047914+Mapkind@users.noreply.github.com>
2025-09-24 16:45:34 -0600

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,22 +1,42 @@
+window.onload = function() {
+    let REGISTRY_ID;
 
-let REGISTRY_ID = "110070391939";
+    const buttonMenu = document.getElementById("buttonMenuDiv");
+    //buttonMenu.style.width = "100px"; div.style.height = "100px";
+    console.log("buttonMenu: ",buttonMenu);
 
-async function getData(){
+    const submitButton = document.createElement('button');
+    submitButton.innerHTML = "GIMME THE DATA";
+    //submitButton.style.width = "100px"; submitButton.style.height = "100px";
+    const idInput = document.createElement("input");
+    idInput.setAttribute("type", "text");
+    idInput.setAttribute("placeholder", "FRS ID");
+    idInput.setAttribute("id", "idInput");
 
-    fetch(`https://echodata.epa.gov/echo/dfr_rest_services.get_dfr?output=JSON&p_id=${REGISTRY_ID}&p_gt5yr=N`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+    //document.body.appendChild(buttonMenu);
+    buttonMenu.appendChild(idInput);
+    buttonMenu.appendChild(submitButton);
+
+    submitButton.addEventListener('click', getData);
+
+    async function getData(){
+
+        if(idInput.value){
+            REGISTRY_ID = idInput.value
+            
+            fetch(`https://echodata.epa.gov/echo/dfr_rest_services.get_dfr?output=JSON&p_id=${REGISTRY_ID}&p_gt5yr=N`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Data: ",data);
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
         }
-        return response.json();
-    })
-    .then(data => {
-        console.log("Data: ",data);
-    })
-    .catch(error => {
-        console.error('Fetch error:', error);
-    });
-
-}
-
-getData();
+    }
+};

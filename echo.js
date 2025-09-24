ea5037ea96602aa559ca57ecd94c0cce540a469f
Mapkind <92047914+Mapkind@users.noreply.github.com>
2025-09-24 16:11:24 -0600

    Update echo.js

--- echo.js
+++ echo.js
@@ -3,21 +3,17 @@ let REGISTRY_ID = "110070391939";
 
 async function getData(){
 
-    fetch(`https://echodata.epa.gov/echo/dfr_rest_services.get_dfr?output=JSON&p_id=${REGISTRY_ID}&p_gt5yr=N`) // Replace with your desired URL
+    fetch(`https://echodata.epa.gov/echo/dfr_rest_services.get_dfr?output=JSON&p_id=${REGISTRY_ID}&p_gt5yr=N`)
     .then(response => {
-        // Check if the request was successful (status code 200-299)
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        // Parse the response body as JSON
         return response.json();
     })
     .then(data => {
-        // Handle the retrieved data
         console.log("Data: ",data);
     })
     .catch(error => {
-        // Handle any errors that occurred during the fetch operation
         console.error('Fetch error:', error);
     });
 
